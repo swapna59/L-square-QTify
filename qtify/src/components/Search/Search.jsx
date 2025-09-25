dee144c1af8f219836b9c748057c43f06c6c48b1
Swapna Katturi <katturi.swapna@gmail.com>
2025-09-25 00:44:08 -0700

    module 1 changes

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./Search.module.css";
 import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
-import useAutocomplete from "@mui/base/useAutocomplete";
+import {useAutocomplete} from "@mui/base";
+
 import { styled } from "@mui/system";
 import { truncate } from "../../helpers/helpers";
 import { useNavigate } from "react-router-dom";
